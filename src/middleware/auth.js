757831dab93ffe84e4ce3a3efb0497ef2fc952c1
danielchochlinski <danielchochlinski@hotmail.com>
2022-06-23 17:31:04 +0200

    Single user

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,12 +3,21 @@ const User = require("../models/user");
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization");
-    console.log(token);
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "daniel");
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+    req.user = user;
+    next();
   } catch (e) {
     res.status(401).send({ error: "Please authenticate." });
   }
-  next();
 };
 
 module.exports = auth;
